9026f7c880c65e1b2b6a5e328eefd352c36ea138
Kanishk Khurana <74613246+KanishkKhurana@users.noreply.github.com>
2023-12-09 17:41:50 +0530

    Update bungee_bridge.py

--- bungee_bridge.py
+++ bungee_bridge.py
@@ -36,57 +36,9 @@ class BungeeBridgeProcessor(GenericProcessorSnapshot):
             self._logger.error('Currently only supports single block height')
             raise Exception('Currently only supports single block height')
 
-        # get txs for this epoch
-        txs_hset = await redis_conn.hgetall(epoch_txs_htable(epoch.epochId))
-        all_txs = {k.decode(): EthTransactionReceipt.parse_raw(v) for k, v in txs_hset.items()}
-
-        contract_address = '0x555A64968E4803e27669D64e349Ef3d18FCa0895'
-        contract_txs = list(
-            map(
-                lambda x: x.dict(), filter(
-                    lambda tx: tx.to == contract_address,
-                    all_txs.values(),
-                ),
-            ),
-        )
-
-        with open('snapshotter/modules/computes/static/abis/bungee_bridge_refuel.json') as f:
-            abi = json.load(f)
-
-        node = rpc_helper.get_current_node()
-        w3 = node['web3_client']
-        contract = w3.eth.contract(address=contract_address, abi=abi)
-
-        eld = EventLogDecoder(contract)
-
-        snapshots = []
+        
+        snapshots = [('0xc3761EB917CD790B30dAD99f6Cc5b4Ff93C4F9eA', BungeeBridgeSnapshot(receiver='sdfsdfs', amount=1242, srcChainTxHash='sdjvskdfksdhf')]
         # min amount in wei
         # min_amount = 2000000000000000
-        min_amount = 0
-        processed_logs = []
-
-        for tx_receipt in contract_txs:
-            for log in tx_receipt['logs']:
-                # Send (address receiver, uint256 amount, bytes32 srcChainTxHash) event log topic
-                if log['topics'][0] == '0x2f824f69f211e444df15d741157e83cdf23c50f39399b9523853a84b91379ca6':
-                    try:
-                        processed_logs.append(eld.decode_log(log))
-                    except:
-                        pass
-        if processed_logs:
-            for log in processed_logs:
-                if log['amount'] > min_amount:
-                    snapshots.append(
-                        (
-                            log['receiver'].lower(),
-                            BungeeBridgeSnapshot(
-                                receiver=log['receiver'],
-                                amount=log['amount'],
-                                srcChainTxHash=log['srcChainTxHash'],
-                            ),
-                        ),
-                    )
-                else:
-                    self._logger.info('Found tx with amount less than min amount, ignoring!')
-
+        
         return snapshots
